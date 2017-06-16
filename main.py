3f921790e04df8208142fbbbe1fa52c2d3a020ba
Rajat Gupta <rajatpunkstaa@gmail.com>
2017-06-16 23:52:47 +0530

    gui-bug-rollback

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ requests.packages.urllib.disable_warnings()
 try:
     unverifiedHttpsContext = ssl._create_unverified_context
 except AttributeError:
-    ssl._create_default_https_context = unverifiedHttpsContext
+    pass
 else:
-
+    ssl._create_default_https_context = unverifiedHttpsContext
 
 mHeaders = {'''user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)
            AppleWebKit/537.36 (KHTML, like Gecko)
