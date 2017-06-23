7c205423c87dd4f9f6c428d1c1a73927eb556394
Rajat Gupta <rajatpunkstaa@gmail.com>
2017-06-23 17:16:02 +0530

    frame-ready

--- engine.py
+++ engine.py
@@ -12,3 +12,19 @@ def speakNow(value):
     engine.say(value)
     engine.runAndWait()
 #--------------End of speakNow--------------
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, pos = wx.DefaultPosition, size = wx.Size(450, 100),style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.CLIP_CHILDREN, title='YODA')
+        panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        label =  wx.StaticText(panel, label = "I'm Yoda !, At your command my lord.")
+        sizer.Add(label, 0, wx.ALL, 5)
+        self.text = wx.TextCtrl(panel, style = wx.TE_PROCESS_ENTER, size = (400, 30))
+        self.text.SetFocus()
+        self.text.Bind(wx.EVT_TEXT_ENTER, self.onEnter)
+        sizer.Add(self.text, 0, wx.ALL, 5)
+        panel.SetSizer(sizer)
+        self.Show()
+        speakNow("Yoda, the most wisest living on this planet, Waiting for your command my lord !")
+        
